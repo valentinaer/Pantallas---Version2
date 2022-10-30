a3e29f3196622b6c55fc430bf74d448c7569cae4
Valentina <rodriguezvalentinae@gmail.com>
2022-10-29 21:09:44 -0300

    Arreglos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using grupoB_TP;
+
 namespace Version_2___Pantallas
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace Version_2___Pantallas
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new AccesoAlSistema());
         }
     }
 }
