7dfa2cf3d57aa6934fe5bcfa7e76d19013816e9c
FRANCISCO LEONEL GALLARINI <01GA42369886@campus.economicas.uba.ar>
2022-11-01 17:15:58 -0300

    Clases

---
+++ Localidad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Version_2___Pantallas
+{
+    internal class Localidad
+    {
+        public List<Localidad> Localidades { get; set; }
+        public string Provincia { get; set; }
+
+    }
+}
---
+++ PaisCiudadInternacional.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Version_2___Pantallas
+{
+    internal class PaisCiudadInternacional
+    {
+
+        public List<PaisCiudadInternacional> PaisCiudadInter { get; set; }
+        public string RegionInter { get; set; }
+
+    }
+}
